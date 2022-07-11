4045029c682f2a4870335f8ad517a65b24741274
un-defined-name <null>
2022-07-12 06:54:12 +0800

    feat(lc/121): best Time to Buy and Sell Stock[Sliding Window]

---
+++ src/lc/121-Best Time to Buy and Sell Stock/index.ts
@@ -0,0 +1,16 @@
+function maxProfit(prices: number[]): number {
+    let l:number = 0
+    let r:number = 0
+    let min:number = Number.MAX_SAFE_INTEGER
+    let maxMoney:number = 0
+    while(r<prices.length){
+        min = Math.min(prices[l],prices[0])
+        maxMoney = Math.max(maxMoney,prices[r]-min)
+        ++l
+        ++r
+
+    }
+    return maxMoney
+};
+
+export default maxProfit
