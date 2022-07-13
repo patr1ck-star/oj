5ff3e17cbcaefd9186216650bc28014a540080e5
un-defined-name <null>
2022-07-13 22:52:01 +0800

    feat(lc/190): reverse Bit[bitwise operation]

---
+++ src/lc/190-Reverse Bits/index.ts
@@ -0,0 +1,11 @@
+function reverseBits(n: number): number {
+    let ans:number = 0
+	for(let i=0;i<32;++i){
+        ans = ans*2 +(n&1)
+        n = n>>>1
+    }
+    return ans
+};
+
+
+export default reverseBits
