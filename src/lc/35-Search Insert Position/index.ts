6cccec0f1ea7c50a57973e58ba4ff9657b73dd44
un-defined-name <null>
2022-07-20 23:17:00 +0800

    feat(lc/35): search Insert Position

---
+++ src/lc/35-Search Insert Position/index.ts
@@ -0,0 +1,15 @@
+function searchInsert(nums: number[], target: number): number {
+    let l:number = 0
+    let r:number = nums.length-1
+    while(l<=r){
+        const mid:number = Math.floor((r-l)/2)+l
+        if(nums[mid]>target){
+            r = mid -1
+        }else if(nums[mid]<target){
+            l = mid +1
+        }else{
+            return mid
+        }
+    }
+    return r+1
+};
