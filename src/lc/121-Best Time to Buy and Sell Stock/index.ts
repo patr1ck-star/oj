aa44008b1bb44f097148d967d65710ae368452be
un-defined-name <null>
2022-07-20 22:20:56 +0800

    fix(lc/121): update 121

--- src/lc/121-Best Time to Buy and Sell Stock/index.ts
+++ src/lc/121-Best Time to Buy and Sell Stock/index.ts
@@ -4,7 +4,7 @@ function maxProfit(prices: number[]): number {
     let min:number = Number.MAX_SAFE_INTEGER
     let maxMoney:number = 0
     while(r<prices.length){
-        min = Math.min(prices[l],prices[0])
+        min = Math.min(prices[l],min)
         maxMoney = Math.max(maxMoney,prices[r]-min)
         ++l
         ++r
