6330dfe71c07932088ad62f474cfba3c267dbdf4
patr1ck-star <null>
2024-06-02 02:37:46 +0800

    style: add prettier

---
+++ .prettierrc.cjs
@@ -0,0 +1,18 @@
+module.exports = {
+    tabWidth: 4,
+    printWidth: 100,
+    semi: true,
+    singleQuote: true,
+    useTabs: false,
+    trailingComma: 'none',
+    bracketSpacing: true,
+    quoteProps: 'as-needed',
+    arrowParens: 'always',
+    rangeStart: 0,
+    rangeEnd: Infinity,
+    requirePragma: false,
+    insertPragma: false,
+    proseWrap: 'preserve',
+    htmlWhitespaceSensitivity: 'css',
+    endOfLine: 'lf'
+};
