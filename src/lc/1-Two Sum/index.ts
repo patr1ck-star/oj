3a52e4f4880bf11452a51549ab263ec86cab1f9e
patr1ck-star <null>
2024-06-02 02:22:53 +0800

    feat(lc/1): two sum

--- src/lc/1-Two Sum/index.ts
+++ src/lc/1-Two Sum/index.ts
@@ -1,14 +1,14 @@
 function twoSum(nums: number[], target: number): number[] {
-    const map:Map<number,number> = new Map<number,number>()
-    for(let i=0;i<nums.length;++i){
-        const cur:number = nums[i]
-        if(!map.has(cur)){
-            map.set(target-cur,i)
-        }else{
-            return [map.get(cur)!,i]
+    const targetMap = new Map<number, number>();
+    for (let i = 0; i < nums.length; ++i) {
+        const cur = nums[i];
+        if (targetMap.has(cur)) {
+            return [targetMap.get(cur)!, i];
         }
+        const diff = target - cur;
+        targetMap.set(diff, i);
     }
-    return []
-};
+    return [-1, -1];
+}
 
-export default twoSum
+export default twoSum;
